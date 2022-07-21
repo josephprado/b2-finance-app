021c58acb88ef6bfe22662a2ce71f9b7851c97aa
prado <pradodje@gmail.com>
2022-07-20 23:48:43 -0700

    Initial commit

---
+++ src/main/java/com/b2/b2data/B2DataApplication.java
@@ -0,0 +1,13 @@
+package com.b2.b2data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2DataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2DataApplication.class, args);
+	}
+
+}
