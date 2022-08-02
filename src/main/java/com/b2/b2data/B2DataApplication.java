c6c5fce52b10342c9ba1cb746c5a39bc6bd0304a
prado <pradodje@gmail.com>
2022-08-02 03:06:10 -0700

    Update entity scan package name

--- src/main/java/com/b2/b2data/B2DataApplication.java
+++ src/main/java/com/b2/b2data/B2DataApplication.java
@@ -1,7 +1,5 @@
 package com.b2.b2data;
 
-import com.b2.b2data.service.TransactionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -10,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.b2.b2data.repository")
-@EntityScan("com.b2.b2data.model")
+@EntityScan("com.b2.b2data.domain")
 //@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class B2DataApplication {
 
